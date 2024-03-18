3e40c7f3049a35ac1f8f87cb2a4f5ac34581e474
chillnchill <109694910+chillnchill@users.noreply.github.com>
2024-03-18 14:55:46 +0200

    Added toastr.js

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using HouseRentingSystem.Web.Infrastructure.Extensions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using VideoGameLibrary.Common;
+using VideoGameLibrary.Services.Data;
 using VideoGameLibrary.Services.Data.Interfaces;
+using VideoGameLibrary.Web.ViewModels.Game;
 
 namespace VideoGameLibrary.Controllers
 {
+    using static VideoGameLibrary.Common.NotificationMessagesConstants;
+
     [Authorize]
     public class GameController : Controller
     {
@@ -13,11 +19,47 @@ namespace VideoGameLibrary.Controllers
             this.gameService = gameService;
         }
 
-        [AllowAnonymous] 
+        [AllowAnonymous]
         public async Task<IActionResult> AllGames()
         {
             var allGames = await gameService.GetAllGamesAsync();
             return View(allGames);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(string id)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                TempData[ErrorMessage] = "You need to be logged in to view game details!";
+                return RedirectToAction("AllGames", "Game");
+            }
+
+            // Continue with the rest of your logic
+            bool gameExists = await gameService.ExistsByIdAsync(id);
+            if (!gameExists)
+            {
+                TempData[ErrorMessage] = "Game with the provided id does not exist!";
+                return RedirectToAction("AllGames", "Game");
+            }
+
+            try
+            {
+                GameDetailsViewModel viewModel = await gameService.GetGameDetailsByIdAsync(id);
+                return View(viewModel);
+            }
+            catch (Exception)
+            {
+                return GeneralError();
+            }
+        }
+
+        private IActionResult GeneralError()
+        {
+            TempData[ErrorMessage] =
+                "Unexpected error occurred! Please try again later or contact administrator";
+
+            return RedirectToAction("AllGames", "Game");
         }
     }
 }
--- obj/Debug/net6.0/VideoGameLibrary.AssemblyInfo.cs
+++ obj/Debug/net6.0/VideoGameLibrary.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoGameLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b99556cb8a0b967b9378e3c0aa98c56b64bfcedb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37076346026661bc33514a5672c37278787181d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoGameLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoGameLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
