b2a2eee444fcddef4aa16615f3735e426d5cd06e
chillnchill <109694910+chillnchill@users.noreply.github.com>
2024-03-21 00:44:52 +0200

    Add method fixed

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -49,65 +49,61 @@ namespace VideoGameLibrary.Controllers
             }
         }
 
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Add(GameFormModel model)
-		{
-            model.OwnerId = User.GetId()!;
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Add(GameFormModel model)
+        {
+            
 
             bool platformsExist =
-				await platformService.ExistsByIdAsync(model.PlatformId);
-			if (!platformsExist)
-			{
-				// Adding model error to ModelState automatically makes ModelState Invalid
-				ModelState.AddModelError(nameof(model.PlatformId), "Selected platform does not exist!");
-			}
+                await platformService.ExistsByIdAsync(model.PlatformId);
+            if (!platformsExist)
+            {
+                // Adding model error to ModelState automatically makes ModelState Invalid
+                ModelState.AddModelError(nameof(model.PlatformId), "Selected platform does not exist!");
+            }
 
-			bool genresExist =
-				await genreService.ExistsByIdAsync(model.GenreId);
-			if (!genresExist)
-			{
-				// Adding model error to ModelState automatically makes ModelState Invalid
-				ModelState.AddModelError(nameof(model.GenreId), "Selected genre does not exist!");
-			}
+            bool genresExist =
+                await genreService.ExistsByIdAsync(model.GenreId);
+            if (!genresExist)
+            {
+                // Adding model error to ModelState automatically makes ModelState Invalid
+                ModelState.AddModelError(nameof(model.GenreId), "Selected genre does not exist!");
+            }
 
             if (!ModelState.IsValid)
             {
                 model.Platform = await platformService.AllPlatformsAsync();
                 model.Genre = await genreService.AllGenresAsync();
 
-
-                foreach (var modelStateEntry in ModelState.Values)
-                {
-                    foreach (var error in modelStateEntry.Errors)
-                    {
-                        // Log or debug error message
-                        Console.WriteLine(error.ErrorMessage);
-                    }
-                }
+                var errors = ModelState
+                    .Where(x => x.Value.Errors.Count > 0)
+                    .Select(x => new { x.Key, x.Value.Errors })
+                    .ToArray();
 
 
                 return View(model);
-			}
+            }
 
-			try
-			{
-				await gameService.AddGameAsync(model);
-				TempData[SuccessMessage] = "Game added successfully!";
+            try
+            {
+                string userId = User.GetId()!;
+                await gameService.AddGameAsync(model, userId);
+                TempData[SuccessMessage] = "Game added successfully!";
                 return RedirectToAction("AllGames", "Game");
-			}
-			catch (Exception)
-			{
-				ModelState.AddModelError(string.Empty, "Unexpected error occurred while trying to add a new game! Please try again later or contact administrator!");
-				model.Platform = await platformService.AllPlatformsAsync();
-				model.Genre = await genreService.AllGenresAsync();
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError(string.Empty, "Unexpected error occurred while trying to add a new game! Please try again later or contact administrator!");
+                model.Platform = await platformService.AllPlatformsAsync();
+                model.Genre = await genreService.AllGenresAsync();
 
-				return View(model);
-			}
-		}
+                return View(model);
+            }
+        }
 
 
-		[HttpGet]
+        [HttpGet]
         public async Task<IActionResult> Details(string id)
         {
             if (!User.Identity.IsAuthenticated)
@@ -125,8 +121,17 @@ namespace VideoGameLibrary.Controllers
             }
 
             try
-            {
+            { 
                 GameDetailsViewModel viewModel = await gameService.GetGameDetailsByIdAsync(id);
+
+                string user = User.GetId()!;
+                var isOwner = await gameService.IsOwnerWithIdCreatorOfGameWithId(id, user);
+
+                if (isOwner)
+                {
+                    viewModel.IsOwner = true;
+                }
+
                 return View(viewModel);
             }
             catch (Exception)
